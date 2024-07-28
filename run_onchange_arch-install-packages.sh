cd6425207fb41415d60bd554f242fedcb612af39
brauni <braunnicolaj@gmail.com>
2024-07-28 14:18:02 +0200

    install

--- run_onchange_arch-install-packages.sh
+++ run_onchange_arch-install-packages.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "PEWPEW"
+ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+ID_LIKE=$(grep -oP '(?<=^ID_LIKE=).+' /etc/os-release | tr -d '"')
+
+if [[ $ID == *"arch"* ]] || [[ $ID_LIKE == "arch" ]]; then
+  if command -v paru &> /dev/null; then
+    paru -S --noconfirm git zsh oh-my-zsh-git zsh-antidote btop htop fastfetch
+  fi
+fi
