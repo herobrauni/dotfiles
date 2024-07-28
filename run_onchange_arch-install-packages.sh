1de88d06a37d265b441747fd288d3973ab40f1e3
brauni <braunnicolaj@gmail.com>
2024-07-28 14:20:48 +0200

    install

--- run_onchange_arch-install-packages.sh
+++ run_onchange_arch-install-packages.sh
@@ -4,6 +4,6 @@ ID_LIKE=$(grep -oP '(?<=^ID_LIKE=).+' /etc/os-release | tr -d '"')
 
 if [[ $ID == *"arch"* ]] || [[ $ID_LIKE == "arch" ]]; then
   if command -v paru &> /dev/null; then
-    paru -S --noconfirm git zsh oh-my-zsh-git zsh-antidote btop htop fastfetch
+    paru -S --noconfirm --needed git zsh oh-my-zsh-git zsh-antidote btop htop fastfetch
   fi
 fi
