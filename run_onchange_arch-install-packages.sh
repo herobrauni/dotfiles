e53162af830b8d72c1af6c75e3db753670d96328
brauni@bara <braunnicolaj@gmail.com>
2024-08-04 15:14:59 +0200

    Update .zshrc Remove arch-install-packages.sh Add install_packages.sh Change attributes of tailscale.sh

--- run_onchange_arch-install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
-ID_LIKE=$(grep -oP '(?<=^ID_LIKE=).+' /etc/os-release | tr -d '"')
-
-if [[ $ID == *"arch"* ]] || [[ $ID_LIKE == "arch" ]]; then
-  if command -v paru &> /dev/null; then
-    paru -S --noconfirm --needed git zsh oh-my-zsh-git zsh-antidote btop htop fastfetch ttf-meslo-nerd-font-powerlevel10k alacritty rbw tailscale
-  fi
-fi
