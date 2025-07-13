e0f1bfe807f5796319b0dd109205301d85525028
herobrauni <braunnicolaj@gmail.com>
2025-07-13 15:28:33 +0200

    update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,3 +6,9 @@ end
 starship init fish | source
 zoxide init fish | source
 atuin init fish | source
+set -gx PATH $PATH $HOME/.krew/bin
+
+function fish_mode_prompt; true; end # Init vscode with a fish_mode_prompt with a non-empty function body
+source (code --locate-shell-integration-path fish)
+
+#string match -q "$TERM_PROGRAM" "vscode"; and . (code --locate-shell-integration-path fish)
