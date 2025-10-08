d3f6ab4025b4122c8f32355fc3847a555f21b6b7
herobrauni <braunnicolaj@gmail.com>
2025-10-08 16:25:47 +0200

    moved_scripts

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-/var/home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+# /var/home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 starship init fish | source
 zoxide init fish | source
 atuin init fish | source
