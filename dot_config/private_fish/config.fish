254ad2dee9fbe9cf395e46662bd0371572c9b616
herobrauni <braunnicolaj@gmail.com>
2025-10-20 16:07:20 +0200

    stuff

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    starship init fish | source
+    zoxide init fish | source
+    atuin init fish | source
 end
 
 # /var/home/linuxbrew/.linuxbrew/bin/brew shellenv | source
-starship init fish | source
-zoxide init fish | source
-atuin init fish | source
 
